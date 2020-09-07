b6942da70ec082e72c3b5a26e8da11a35f055a0e
dev-carlos garcia <carlosrenegarcia.a4b@gmail.com>
2020-09-07 14:04:54 +0200

    Convert ordersummary from functinal  component to  class based  component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import logoImage from '../../assests/images/bcode_logo.svg';
 import classes from './Logo.module.css';
 
 const logo=(props)=>(
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height:props.height}}>
         <img src={logoImage}  alt="logo"/>
     </div>
 )
